290b547167ea7aa2e3561c418f049449549c8fbf
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-30 19:30:22 -0500

    first game added to

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,5 +1,13 @@
 class ParamsGamesController < ApplicationController
   def game
+    @name = ''
+    @victory = ''
     @message = params[:message]
+    if @message != nil
+      @name = @message
+    end
+    if @name.upcase.start_with? "A"
+      @victory = 'Your name starts with the first letter of the alphabet!'
+    end
   end
 end
