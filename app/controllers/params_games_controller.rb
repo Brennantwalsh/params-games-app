24dd8be9182345639dddf54c950d2a651ff06590
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-30 19:52:11 -0500

    number-game-added

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -3,11 +3,26 @@ class ParamsGamesController < ApplicationController
     @name = ''
     @victory = ''
     @message = params[:message]
-    if @message != nil
+    unless @message.nil?
       @name = @message
     end
-    if @name.upcase.start_with? "A"
+    if @name.upcase.start_with?("A")
       @victory = 'Your name starts with the first letter of the alphabet!'
+    end
+  end
+
+  def number_game
+    @answer = ''   
+    @message = params[:message]
+    @number = 36
+    unless @message.nil?
+      if @message.to_i > @number
+        @answer = "The number is lower"
+      elsif @message.to_i < @number
+        @answer = "The number is higher"
+      elsif @message.to_i == @number
+        @answer = "you guessed it"
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/params_game' => 'params_games#game'
+  get '/number_game' => 'params_games#number_game'
 end
